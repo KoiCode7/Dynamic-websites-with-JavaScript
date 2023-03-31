b254e1d27c3e1fcdf551558f87a583122e25665e
github <github>
2023-03-31 12:37:58 -0400

    Upload my project

---
+++ app.js
@@ -0,0 +1,20 @@
+const circle = document.getElementById("circle");
+const upBtn = document.getElementById("upBtn");
+const downBtn = document.getElementById("downBtn");
+
+let rotateValue = circle.style.transform;
+let rotateSum;
+
+upBtn.onclick = function () {
+/* Rotate 90 degrees counterclockwise */
+rotateSum = rotateValue + "rotate(-90deg)";
+circle.style.transform = rotateSum;
+rotateValue = rotateSum;
+};
+
+downBtn.onclick = function () {
+/* Rotate 90 degrees clockwise */
+rotateSum = rotateValue + "rotate(90deg)";
+circle.style.transform = rotateSum;
+rotateValue = rotateSum;
+}
